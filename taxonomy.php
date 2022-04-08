6d0288ece4c595b398346753ade99db00c2ffe99
Melvin Lemoine <pro@melvin-lemoine.me>
2022-04-08 20:33:48 +0200

    ✨ Developed taxonomy template

---
+++ taxonomy.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<main class="main">
+    <div class="container">
+        <h1 class="title fs-10 mb-5">Catégorie : <b>Science-Fiction</b></h1>
+
+        <div class="list">
+            <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/mandalorian.jpg')">
+            </a>
+            <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/tron.jpg')">
+            </a>
+            <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/vaiana.jpg')">
+            </a>
+            <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/avatar.jpg')">
+            </a>
+            <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/strange.jpg')">
+            </a>
+            <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/boba.jpg')">
+            </a>
+            <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/vikings.webp')">
+            </a>
+        </div>
+    </div>
+
+</main>
+<?php get_footer(); ?>
