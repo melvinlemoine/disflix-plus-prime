f2dadfa296ea341c45dbb6d0c126af19a9ca3ca0
Melvin Lemoine <pro@melvin-lemoine.me>
2022-04-08 19:09:04 +0200

    🧹 Cleaned template

--- functions.php
+++ functions.php
@@ -17,38 +17,9 @@ function insert_css() {
 	//wp_enqueue_script( 'jquery2' );
 }
 
-add_theme_support( 'menus' );
+add_theme_support('menus');
 
-register_nav_menus( [
+register_nav_menus([
 	'main-menu'  => 'Main navigation',
 	'footer-menu'     => 'Footer navigation'
-] );
-
-add_theme_support( 'post-thumbnails' );
-
-function create_post_type() {
-	register_post_type( 'items',
-		array(
-			'label'              => __( 'Items' ),
-			'singular_label'     => __( 'Item' ),
-			'add_new_item'       => __( 'Add item' ),
-			'edit_item'          => __( 'Edit item' ),
-			'new_item'           => __( 'New item' ),
-			'view_item'          => __( 'View item' ),
-			'search_items'       => __( 'Search items' ),
-			'not_found'          => __( 'Not found réalisation trouvée' ),
-			'not_found_in_trash' => __( 'Not found in trash' ),
-			'public'             => true,
-			'show_ui'            => true,
-			'capability_type'    => 'post',
-			'has_archive'        => true,
-			'hierarchical'       => true,
-			'menu_icon'          => 'dashicons-screenoptions',
-			'taxonomies'         => array( 'types' ),
-			'supports'           => array( 'title', 'editor', 'thumbnail' ),
-			'rewrite'            => array( 'slug' => 'items', 'with_front' => true )
-		)
-	);
-}
-
-add_action( 'init', 'create_post_type' );
+]);
