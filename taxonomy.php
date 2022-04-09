7bf514702e7ae7e5aff2d806d6b49be7dde34bc5
Melvin Lemoine <pro@melvin-lemoine.me>
2022-04-10 00:34:45 +0200

    💄 Improved back button on taxonomy template

--- taxonomy.php
+++ taxonomy.php
@@ -2,8 +2,15 @@
 
 <main class="main">
     <div class="container">
-        <a href="http://localhost/streamify">Retour</a>
-        <h1 class="title fs-10 mb-5">Catégorie : <b>Science-Fiction</b></h1>
+
+        <a href="http://localhost/streamify" class="button has-icon is-white">
+            <span class="icon">
+                <i class="fa-solid fa-chevron-left"></i>
+            </span>
+            Accueil
+        </a>
+
+        <h1 class="title fs-10 mv-5">Catégorie : <b>Science-Fiction</b> </h1>
 
         <div class="list">
             <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/mandalorian.jpg')">
@@ -20,8 +27,10 @@
             </a>
             <a class="ShowCard bg-center bg-cover bg-norepeat" href="#" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/shows/vikings.webp')">
             </a>
+
         </div>
     </div>
 
 </main>
+
 <?php get_footer(); ?>
