330f9ff974ebfab37fec43aaeeef9e0fa2781780
Melvin Lemoine <pro@melvin-lemoine.me>
2022-08-23 19:36:50 +0200

    Fixed mouse move on player

--- single.php
+++ single.php
@@ -179,6 +179,8 @@
 <script>
     console.log("JS");
 
+    let moveTimer;
+
     function PlayVideo(video_url, poster_url, title, episode) {
         console.log("Launch Video !");
         console.log("video_url : " + video_url);
@@ -186,26 +188,27 @@
         $("#body").append("<div class='Player' id='Player'></div>");
         $("#Player").append("<video class='player' id='video' class='header__video' controls data-poster='" + poster_url + "'></video>");
         $("#video").append("<source src='" + video_url + "' type='video/mp4'>");
-        $("#Player").append("<script>player = new Plyr('#video', {title: 'temp',fullscreen: {enabled: true, fallback: true, iosNative: false, container: '#main'}})<\/script>");
+        $("#Player").append("<script>player = new Plyr('#video', {title: 'temp',fullscreen: {enabled: true, fallback: true, iosNative: false, container: '#main'}, hideControls: false})<\/script>");
         $("#Player").append("<script>player.on('controlsshown', (event) => { $('#informations').addClass('shown');$('#informations').removeClass('hidden') });<\/script>");
         $("#Player").append("<script>player.on('controlshidden', (event) => { $('#informations').addClass('hidden');$('#informations').removeClass('shown') });<\/script>");
         $("#Player").append("<script>$('#informations').mouseenter(function(){$('#informations').addClass('shown')})<\/script>");
-        $("#Player").append("<div class='Player__informations' id='informations' onmouseenter='showControls()'> \
+        $("#Player").append("<script>$('#Player').on('mousemove',function(){console.log('Im moving!');player.toggleControls(true);clearTimeout(moveTimer); moveTimer = setTimeout(function() {console.log('I stopped moving!');console.log('The mouse is not moving.');player.toggleControls(false);}, 1000) })<\/script>");
+            $("#Player").append("<div class='Player__informations' id='informations' onmouseenter='showControls()'> \
         <i class='Player__informations_back fa-solid fa-arrow-left' onclick='CloseVideo()'></i> \
         <p class='Player__informations_title'>" + title + "</p>\
         <p class='Player__informations_episode'>" + episode + "</p>\
         \
         </div>");
-    }
+        }
 
-    function CloseVideo() {
-        $('#Player').remove();
-    }
+        function CloseVideo() {
+            $('#Player').remove();
+        }
 
-    function showControls() {
-        console.log('show controls');
-        player.toggleControls(true);
-    }
+        function showControls() {
+            console.log('show controls');
+            player.toggleControls(true);
+        }
 </script>
 
 <script src='<?php bloginfo('template_url'); ?>/assets/js/plyr.min.js'></script>
